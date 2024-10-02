c35e47fe58c6ecba8fe15da9b0e5120a2bc11c13
4121nnn <4121nnn@gmail.com>
2024-10-02 10:51:41 +0300

    add screenshot

--- back/src/main/java/io/nuri/streams/JavaStreams.java
+++ back/src/main/java/io/nuri/streams/JavaStreams.java
@@ -1,13 +1,17 @@
 package io.nuri.streams;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class JavaStreams {
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(JavaStreams.class, args);
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(JavaStreams.class)
+				.bannerMode(Banner.Mode.OFF)
+				.run(args);
 	}
 }
 
